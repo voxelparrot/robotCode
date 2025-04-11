e00379c44eb89d1de7ebeb5ceaa64a1b218594aa
Leon <126285905+leoniscooler@users.noreply.github.com>
2025-04-11 15:22:53 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,25 @@
+#bot1
+def pickUpPoms():
+  times = 5
+  while times > 0:
+    set_servo_position(,) #claw down
+    set_servo_position(,) #claw close
+    set_servo_position(,) #claw up
+    set_servo_position(,) #claw open
+    times--
+  
+def goToPoms1():
+  forward()
+  left()
+  forward()
+  right()
+  forward()
+  left()
+
+def putPomsInTray():
+  
+  
+enable_servos()
+goToPoms1()
+
 
