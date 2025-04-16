df4a0ac73a6184fd95eb3beb7a1435c9a09843eb
voxelparrot <voxelparrot@gmail.com>
2025-04-16 09:17:50 -0700

    Update robot.py
    describing path of bot 1

--- robot.py
+++ robot.py
@@ -1,13 +1,12 @@
 #bot1
-def pickUpPoms():
-  times = 5
-  while times > 0:
-    set_servo_position(,) # Move claw down
-    set_servo_position(,) # Close claw
-    set_servo_position(,) # Move claw up
-    set_servo_position(,) # Open claw
-    times--
-  
+
+
+# Move forward 
+# Turn left 45º
+# Move forward
+# Turn right 90º
+# Move forward to center
+# Turn left 90º
 def goToPoms(): # Position the bot to grab the poms
   forward()
   left()
@@ -15,22 +14,36 @@ def goToPoms(): # Position the bot to grab the poms
   right()
   forward()
   left()
-
+def pickUpPoms(): # Pick up top 3 poms
+  times = 3
+  while times > 0:
+    set_servo_position(,) # Move claw down
+    set_servo_position(,) # Close claw
+    set_servo_position(,) # Move claw up
+    set_servo_position(,) # Open claw
+    times--
+# Move forward
+# Turn left 90º
+# Move forward
+# Turn and pick up bottles with bottle claw
+# Turn left 90º
+# Move forward
 def putPomsInTray(): # Deposit the poms into the tray
-  
-  
+# Pick up and deposit the rest of the poms (optional)
+# Turn left 90º
+# Move forward
+# Grab pickle
+# Turn left 90º
+# Move forward
+# Turn right 90º
+# Pick up tomato
+# Turn around 180º
+# Move forward
+# Turn left 90º
+# Move forward
+# Turn right 90º
+# Move forward
+
 enable_servos()
 goToPoms()
-
-
-
-
-
-
-
-
-
-
-
-
 
