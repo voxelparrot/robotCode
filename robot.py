009b1d7c7e59548e87527191cae72f61d5921fd7
voxelparrot <voxelparrot@gmail.com>
2025-04-25 15:48:03 -0700

    remove bot 1

--- robot.py
+++ robot.py
@@ -45,26 +45,3 @@ def putPomsInTray(): # Deposit the poms into the tray
 
 enable_servos()
 goToPoms()
-
-
-#bot2
-
-# Move forward 
-# Pick up cups
-# Turn left 90º
-# Move backward
-# Use rotating claw to grab drink
-# Use rotating claw to put drink into first cup
-# Use rotating claw to grab ice
-# Use rotating claw to put ice into first cup
-# Use rotating claw to grab drink
-# Use rotating claw to put drink into second cup
-# Use rotating claw to grab ice
-# Use rotating claw to put ice into second cup
-# Use rotating claw to grab drink
-# Use rotating claw to put drink into third cup
-# Use rotating claw to grab ice
-# Use rotating claw to put ice into third cup
-# Move forward
-# Turn right 90º
-# Move forward
